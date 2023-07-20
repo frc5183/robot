8f124c5a423f524e07c002fe1cd5e9b90a81facf
TripTripy <53990651+TripTripy@users.noreply.github.com>
2023-07-20 11:58:47 -0400

    Implement ExponentialCurve
    TODO:
    Implement any hardware classes.
    Implement generic subsystems.
    Implement generic commands
    Implement TupleControl and generic derivatives
    Implement generic derivatives of SingleControl
    Implement several different Curves including Quadratic, Square Root, Linear, etc. Also implement a Curve that allows piecewise combinations of other Curves

---
+++ src/main/java/frc/robot/control/curve/ExponentialCurve.java
@@ -0,0 +1,23 @@
+package frc.robot.control.curve;
+
+/**
+ * An implementation of Curve that utilizes an exponential curve
+ */
+public class ExponentialCurve extends Curve {
+    private double exaggeration=1.0;
+    @Override
+    public double internal_curve(double x) {
+        if (x==0) {
+            return 0;
+        }
+        return (x / Math.abs(x)) * ((Math.pow(1 + exaggeration, Math.abs(x))) - 1) / exaggeration;
+    }
+
+    /**
+     * Used to set the expoential exaggeration value
+     * @param exaggeration The new exponential exaggeration value
+     */
+    public void setExaggeration(double exaggeration) {
+        this.exaggeration = exaggeration;
+    }
+}
