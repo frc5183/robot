5a7580d3eeaa959edb72cdaa44f99a68a5d21ad6
TripTripy <53990651+TripTripy@users.noreply.github.com>
2023-07-20 13:11:54 -0400

    StickMode update: Adds getStickValue
    TODO:
    Implement any hardware classes.
    Implement generic subsystems.
    Implement generic commands
    Implement TupleControl and generic derivatives
    Implement generic derivatives of SingleControl

--- src/main/java/frc/robot/control/enumeration/StickMode.java
+++ src/main/java/frc/robot/control/enumeration/StickMode.java
@@ -1,5 +1,7 @@
 package frc.robot.control.enumeration;
 
+import edu.wpi.first.wpilibj.XboxController;
+
 /**
  * Represents an available "Stick" or "Axis" on an XBOX 360 Controller
  * "Sticks" should be represented by any input that could vary from -1 to 1
@@ -16,5 +18,44 @@ public enum StickMode {
     /**
      * Represents an adaptation of the DPAD/HAT's up and down buttons to become a digital axis
      */
-    HATY
+    HATY;
+
+    /**
+     * Used to get the value of a StickMode using an XboxController
+     * @param mode the StickMode to be evaluated
+     * @param xbox the XboxController to retrieve the value from
+     * @return the value of the StickMode
+     */
+    public static double getStickValue(StickMode mode, XboxController xbox) {
+        switch(mode) {
+            case LEFTX:
+                return xbox.getLeftX();
+            case LEFTY:
+                return xbox.getLeftY();
+            case RIGHTX:
+                return xbox.getRightX();
+            case RIGHTY:
+                return xbox.getRightY();
+            case HATY:
+                int x = xbox.getPOV();
+                if (x==0 || x==45 || x==315) {
+                    return 1;
+                } else if (x==180 || x==135 || x==225) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            case HATX:
+                int y = xbox.getPOV();
+                if (y==90 || y==45 || y==135) {
+                    return 1;
+                } else if (y==270 || y==315 || y==225) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            default:
+                return 0;
+        }
+    }
 }
