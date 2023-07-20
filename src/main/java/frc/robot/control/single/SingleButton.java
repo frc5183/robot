81c8628a6f1cdca6cc519efd3fd5a819aa6cc997
TripTripy <53990651+TripTripy@users.noreply.github.com>
2023-07-20 13:38:42 -0400

    Update SingleButton
    TODO:
    Implement any hardware classes.
    Implement generic subsystems.
    Implement generic commands
    Implement TupleControl and generic derivatives
    Implement generic derivatives of SingleControl

---
+++ src/main/java/frc/robot/control/single/SingleButton.java
@@ -0,0 +1,62 @@
+package frc.robot.control.single;
+
+import frc.robot.control.enumeration.Button;
+import frc.robot.control.enumeration.ButtonStyle;
+
+/**
+ * A SingleControl that uses a sole Button with two configurable values for pressed and unpressed
+ * ButtonStyle can be used to change the behavior as the following:
+ * Switch works like a normal switch, if the physical button is pressed, it returns pressed, and vice versa.
+ * Set changes the value it returns. Every time the physical button is released, the value it returns is changed. This can be used so you do not have to hold the physical button.
+ */
+public class SingleButton extends SingleControl {
+    private ButtonStyle style;
+    private Button button;
+    private double unpressed=0;
+    private double pressed=1;
+    private boolean swapped=false;
+    private boolean lastState=false;
+    @Override
+    public double getValue() {
+        boolean buttonVal=Button.getButtonValue(button, xbox);
+        switch (style) {
+            case SWITCH:
+                lastState=buttonVal;
+                if (buttonVal) {
+                    swapped=true;
+                    return pressed;
+                } else {
+                    swapped=false;
+                    return unpressed;
+                }
+            case SET:
+                if (!lastState && buttonVal) {
+                    swapped=!swapped;
+                }
+                lastState=buttonVal;
+                if (swapped) {
+                    return pressed;
+                } else {
+                    return unpressed;
+                }
+            default:
+                return 0;
+        }
+    }
+
+    public void setStyle(ButtonStyle style) {
+        this.style = style;
+    }
+
+    public void setButton(Button button) {
+        this.button = button;
+    }
+
+    public void setPressed(double pressed) {
+        this.pressed = pressed;
+    }
+
+    public void setUnpressed(double unpressed) {
+        this.unpressed = unpressed;
+    }
+}
