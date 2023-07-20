b21ae5c321dc51840f74e936ff039a34625a69dd
TripTripy <53990651+TripTripy@users.noreply.github.com>
2023-07-20 13:38:18 -0400

    Update Button
    TODO:
    Implement any hardware classes.
    Implement generic subsystems.
    Implement generic commands
    Implement TupleControl and generic derivatives
    Implement generic derivatives of SingleControl

--- src/main/java/frc/robot/control/enumeration/Button.java
+++ src/main/java/frc/robot/control/enumeration/Button.java
@@ -1,8 +1,45 @@
 package frc.robot.control.enumeration;
 
+import edu.wpi.first.wpilibj.XboxController;
+
 /**
  * Represents the available physical buttons on an XBOX 360 Controller
  */
 public enum Button {
-    A, B, X, Y, LEFTBUMPER, RIGHTBUMPER, LEFTSTICK, RIGHTSTICK, SELECT, START
+    A, B, X, Y, LEFTBUMPER, RIGHTBUMPER, LEFTSTICK, RIGHTSTICK, SELECT, START;
+
+    /**
+     * Used to evaluate a Button with an XboxController to retrieve the status of the Physical Button
+     * On that Controller. A value of True indicates the button is pressed down.
+     * @param button the Button that needs to be evaluated
+     * @param xbox The XboxController that is used to retrieve the value
+     * @return
+     */
+    public static boolean getButtonValue(Button button, XboxController xbox) {
+        switch (button) {
+            case A:
+                return xbox.getAButton();
+            case B:
+                return xbox.getBButton();
+            case X:
+                return xbox.getXButton();
+            case Y:
+                return xbox.getYButton();
+            case START:
+                return xbox.getStartButton();
+            case SELECT:
+                return xbox.getBackButton();
+            case LEFTBUMPER:
+                return xbox.getLeftBumper();
+            case LEFTSTICK:
+                return xbox.getLeftStickButton();
+            case RIGHTBUMPER:
+                return xbox.getRightBumper();
+            case RIGHTSTICK:
+                return xbox.getRightStickButton();
+            default:
+                return false;
+        }
+    }
+
 }
