b8d656c86fa9e9f98ee7ad602e445ff6a84260fc
TripTripy <53990651+TripTripy@users.noreply.github.com>
2023-07-20 11:58:26 -0400

    Add Deadzone Checking to abstract Curve
    TODO:
    Implement any hardware classes.
    Implement generic subsystems.
    Implement generic commands
    Implement TupleControl and generic derivatives
    Implement generic derivatives of SingleControl
    Implement several different Curves including Quadratic, Exponential, Square Root, Linear, etc. Also implement a Curve that allows piecewise combinations of other Curves

--- src/main/java/frc/robot/control/curve/Curve.java
+++ src/main/java/frc/robot/control/curve/Curve.java
@@ -6,9 +6,31 @@ package frc.robot.control.curve;
 public abstract class Curve {
 
     /**
+     * Represents the value that under which inputs should be ignored and return 0
+     * This is to prevent "stick drift"
+     */
+    private double deadzone = 0.09;
+    /**
      * Implementations turn one double number into another
      * @param x the input to the curved
      * @return the output, processed value of the curve
      */
-    public abstract double curve(double x);
+    protected abstract double internal_curve(double x);
+
+    /**
+     * Returns an implementation's curve while also accounting for a deadzone
+     * @param x the input to be curved
+     * @return the output, processed value of the curve
+     */
+    private double curve(double x) {
+        if (Math.abs(x)<deadzone) {
+            return internal_curve(x);
+        } else {
+            return internal_curve(x);
+        }
+    }
+
+    public void setDeadzone(double deadzone) {
+        this.deadzone = deadzone;
+    }
 }
