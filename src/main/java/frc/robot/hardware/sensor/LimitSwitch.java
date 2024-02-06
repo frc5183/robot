730ef10e7b5e27531abfdcaba25591c161703549
Bacon <iam@baconing.tech>
2024-02-06 12:10:15 -0500

    feat(hardware/sensors/logging): implement logging on hardware sensors

--- src/main/java/frc/robot/hardware/sensor/LimitSwitch.java
+++ src/main/java/frc/robot/hardware/sensor/LimitSwitch.java
@@ -1,26 +1,52 @@
 package frc.robot.hardware.sensor;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.Logger;
+
+import java.util.UUID;
 
 public class LimitSwitch {
+    private final UUID id = UUID.randomUUID();
     private final DigitalInput limiter;
-    private final boolean reverse;
+    private final boolean reversed;
+    /**
+     * @param id The digital IO port that the limit switch is on
+     * @param reversed whether the limit switch is reversed
+     */
+    public LimitSwitch(int id, boolean reversed) {
+        limiter= new DigitalInput(id);
+        this.reversed = reversed;
+
+        Logger.append(Logger.LogType.HardwareSensor, "limitSwitch/" + this.getId() + "/channel", limiter.getChannel());
+        Logger.append(Logger.LogType.HardwareSensor, "limitSwitch/" + this.getId() + "/reversed", reversed);
+
+        Logger.append(Logger.LogType.HardwareSensor, "limitSwitch/" + this.getId(), "New LimitSwitch created with channel: " + limiter.getChannel() + " and reversed? " + (reversed ? "yes" : "no"));
+    }
+
     /**
      * @param id The digital IO port that the limit switch is on
      */
-    public LimitSwitch(int id, boolean reverse) {
-        limiter= new DigitalInput(id);
-        this.reverse=reverse;
-    }
     public LimitSwitch(int id) {
         limiter=new DigitalInput(id);
-        this.reverse=false;
+        this.reversed =false;
+
+        Logger.append(Logger.LogType.HardwareSensor, "limitSwitch/" + this.getId() + "/channel", limiter.getChannel());
+        Logger.append(Logger.LogType.HardwareSensor, "limitSwitch/" + this.getId() + "/reversed", reversed);
+
+        Logger.append(Logger.LogType.HardwareSensor, "limitSwitch/" + this.getId(), "New LimitSwitch created with channel: " + limiter.getChannel());
     }
+
     public boolean isClosed() {
-        if (reverse) {
+        if (reversed) {
+            Logger.append(Logger.LogType.HardwareSensor, "limitSwitch/" + this.getId() + "/closed", !limiter.get());
             return !limiter.get();
         } else {
+            Logger.append(Logger.LogType.HardwareSensor, "limitSwitch/" + this.getId() + "/closed", limiter.get());
             return limiter.get();
         }
+    }
+
+    public UUID getId() {
+        return id;
     }
 }
