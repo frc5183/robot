4158ce9e7e795bec4028d5ee75006790316bf5d1
Trip-kun <kaidenmcmartin1@gmail.com>
2024-03-03 23:57:14 -0500

    feat(robot): Adds season code, not yet finished

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -1,11 +1,20 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.control.AutonomousButtonMapper;
+import frc.robot.control.command.*;
 import frc.robot.control.curve.ExponentialCurve;
+import frc.robot.control.enumeration.Button;
 import frc.robot.control.enumeration.StickMode;
 import frc.robot.control.single.HalfStick;
+import frc.robot.control.single.SingleButton;
 import frc.robot.control.single.SingleControl;
+import frc.robot.control.single.SingleTwoPhaseButton;
 import frc.robot.control.tuple.CombinedTuple;
 import frc.robot.control.tuple.TupleControl;
+import frc.robot.math.MecanumDriveOdometryWrapper;
+import frc.robot.subsystem.GenericMecanumDrive;
+import frc.robot.subsystem.GenericSpinner;
 
 /**
  * A class made simply to hold configuration for the robot.
@@ -20,7 +29,7 @@ public class Config {
      * Represents the ID of the first controller as represented in the Driver Station Configuration
      */
     public static final int SecondControllerID = 1;
-
+    public static ControllerManager controllerManager = new ControllerManager();
     /**
      * Represents the diameter of the wheels in inches.
      */
@@ -45,16 +54,56 @@ public class Config {
      */
     public static final int shooterMotor = 0;
     /**
-     * Represents the ID of the Feed Motor
+     * Represents the ID of the Floor Motor
      */
-    public static final int feedMotor = 0;
+    public static final int floorMotor = 0;
+    /**
+     * Represents the ID of the Intake Motor
+     */
+    public static final int intakeMotor = 0;
+    /**
+     * Represents the ID of the Elevator Motor
+     */
+    public static final int elevatorMotor = 0;
+    /**
+     * Mecanum Settings
+     */
+    public static final GenericMecanumDrive.MecanumMode mecanumMode = GenericMecanumDrive.MecanumMode.ABSOLUTE;
+    public static final MecanumDriveOdometryWrapper.MecanumWheelPositions mecanumWheels = new MecanumDriveOdometryWrapper.MecanumWheelPositions(
+            new Translation2d(10.125, 9),
+            new Translation2d(-10.125, 9),
+            new Translation2d(10.125, -9),
+            new Translation2d(-10.125, -9)
+    );
     public static final ExponentialCurve driveCurve = new ExponentialCurve();
     static {
         driveCurve.setExaggeration(100);
     }
-    public static final SingleControl botX = new HalfStick(StickMode.LEFTY, driveCurve);
-    public static final SingleControl botY = new HalfStick(StickMode.LEFTX, driveCurve);
-    public static final SingleControl botTurn = new HalfStick(StickMode.RIGHTX, driveCurve);
-    public static final TupleControl translateBot = new CombinedTuple(botX, botY);
+    public static final SingleControl botX = new HalfStick(StickMode.LEFTY, driveCurve).setXboxController(controllerManager.getFirstController());
+    public static final SingleControl botY = new HalfStick(StickMode.LEFTX, driveCurve).setXboxController(controllerManager.getFirstController());
+    public static final SingleControl botTurn = new HalfStick(StickMode.RIGHTX, driveCurve).setXboxController(controllerManager.getFirstController());
+    public static final TupleControl translateBot = new CombinedTuple(botX, botY).setXboxController(controllerManager.getFirstController());
+    public static final SingleControl botIntake = new HalfStick(StickMode.TRIGGER, driveCurve).setXboxController(controllerManager.getSecondController());
+    public static final SingleControl botElevator = new HalfStick(StickMode.LEFTY, driveCurve).setXboxController(controllerManager.getSecondController());
+    public static final SingleControl botFloor = new HalfStick(StickMode.RIGHTX, driveCurve).setXboxController(controllerManager.getSecondController());
 
+    public static Command shoot(GenericSpinner shooter, GenericSpinner intake) {
+        return new CommandGroup(
+        new RunSpinner(shooter, false, 4),
+        new TimedConsumerCommand(intake, 2),
+        new DelayedCommand(new RunSpinner(intake, false, 2), 2)
+        );
+    }
+    public static Command highIntake(GenericSpinner shooter, GenericSpinner intake) {
+        return new CommandGroup(
+                new RunSpinner(intake, true, 3),
+                new RunSpinner(shooter, true, 3)
+        );
+    }
+    public static AutonomousButtonMapper shootButton(GenericSpinner shooter, GenericSpinner intake) {
+        return new AutonomousButtonMapper(() -> shoot(shooter, intake), controllerManager.getSecondController(), Button.A);
+    }
+    public static AutonomousButtonMapper highIntakeButton(GenericSpinner shooter, GenericSpinner intake) {
+        return new AutonomousButtonMapper(() -> highIntake(shooter, intake), controllerManager.getSecondController(), Button.B);
+    }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,14 +6,25 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkLowLevel;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import frc.robot.control.Scheduler;
+import frc.robot.control.command.TeleopMecanum;
+import frc.robot.control.command.TeleopSpinner;
+import frc.robot.hardware.gyro.ADISAxisGyroscope;
+import frc.robot.hardware.gyro.SingleAxisGyroscope;
 import frc.robot.hardware.motor.SparkMaxMotor;
+import frc.robot.hardware.motor.TalonFXMotor;
+import frc.robot.hardware.motor.TalonSRXMotor;
 import frc.robot.hardware.motor.VictorSPXMotor;
+import frc.robot.subsystem.GenericMecanumDrive;
+import frc.robot.subsystem.GenericSpinner;
 
 
 /**
@@ -29,31 +40,70 @@ public class Robot extends TimedRobot
     private SparkMaxMotor leftFront;
     private SparkMaxMotor rightFront;
     private final XboxController controller = new XboxController(0);
-    private MecanumDrive drive;
+    private GenericMecanumDrive drive;
+    private SparkMaxMotor floorMotor;
+    private TalonSRXMotor intakeMotor;
+    private TalonSRXMotor shooterMotor;
+    private TalonFXMotor elevatorMotor;
+    private GenericSpinner shooter;
+    private GenericSpinner intake;
+    private GenericSpinner elevator;
+    private GenericSpinner floor;
+    private SingleAxisGyroscope gyro;
+    public static final Scheduler scheduler = new Scheduler();
 
     @Override
     public void robotInit()
     {
-        leftRear=new SparkMaxMotor(0, CANSparkLowLevel.MotorType.kBrushless);
-        rightRear=new SparkMaxMotor(1, CANSparkLowLevel.MotorType.kBrushless);
-        leftFront=new SparkMaxMotor(2, CANSparkLowLevel.MotorType.kBrushless);
-        rightFront=new SparkMaxMotor(3, CANSparkLowLevel.MotorType.kBrushless);
-        drive = new MecanumDrive(leftFront, leftRear, rightFront, rightRear);
+        leftRear=new SparkMaxMotor(Config.rearLeftMotorID, CANSparkLowLevel.MotorType.kBrushless);
+        rightRear=new SparkMaxMotor(Config.rearRightMotorID, CANSparkLowLevel.MotorType.kBrushless);
+        leftFront=new SparkMaxMotor(Config.frontLeftMotorID, CANSparkLowLevel.MotorType.kBrushless);
+        rightFront=new SparkMaxMotor(Config.frontRightMotorID, CANSparkLowLevel.MotorType.kBrushless);
+        GenericMecanumDrive.MecanumMode mode = Config.mecanumMode;
+        drive = new GenericMecanumDrive(leftRear.getEncodedMotor(), leftFront.getEncodedMotor(), rightFront.getEncodedMotor(), rightRear.getEncodedMotor(), gyro, mode, Config.mecanumWheels, new Pose2d()) ;
+        floorMotor = new SparkMaxMotor(Config.floorMotor, CANSparkLowLevel.MotorType.kBrushless);
+        intakeMotor = new TalonSRXMotor(Config.intakeMotor);
+        shooterMotor = new TalonSRXMotor(Config.shooterMotor);
+        elevatorMotor = new TalonFXMotor(Config.elevatorMotor);
+        shooter = new GenericSpinner(shooterMotor);
+        intake = new GenericSpinner(intakeMotor);
+        elevator = new GenericSpinner(elevatorMotor);
+        floor = new GenericSpinner(floorMotor);
+        gyro = new ADISAxisGyroscope(new ADIS16448_IMU(), SingleAxisGyroscope.Axis.ROLL); // Roll because vertical Roborio
     }
-
+    @Override
+    public void teleopInit()
+    {
+        scheduler.forceEnd();
+        TeleopMecanum teleopMecanum = new TeleopMecanum(drive);
+        scheduler.scheduleCommand(teleopMecanum);
+        TeleopSpinner teleopIntake = new TeleopSpinner(intake, Config.botIntake);
+        scheduler.scheduleCommand(teleopIntake);
+        TeleopSpinner teleopElevator = new TeleopSpinner(elevator, Config.botElevator);
+        scheduler.scheduleCommand(teleopElevator);
+        TeleopSpinner teleopFloor = new TeleopSpinner(floor, Config.botFloor);
+        scheduler.scheduleCommand(teleopFloor);
+    }
 
     @Override
     public void teleopPeriodic()
     {
-        double y = controller.getLeftY();
-        double x = controller.getLeftX();
-        double turn = controller.getRightX();
-        drive.driveCartesian(-y, -x, -turn); // Inversion to follow the right hand rule
+
+        scheduler.run();
+    }
+    @Override
+    public void autonomousInit() {
+        // TODO: Add autonomous code
+    }
+    @Override
+    public void autonomousPeriodic() {
+        scheduler.run();
     }
     public void periodic() {
-        leftRear.periodic();;
-        rightRear.periodic();
-        leftFront.periodic();
-        rightFront.periodic();
+        drive.periodic();
+        shooter.periodic();
+        intake.periodic();
+        elevator.periodic();
+        floor.periodic();
     }
 }
