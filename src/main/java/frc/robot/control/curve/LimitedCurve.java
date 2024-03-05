df379ce57ded6554023f292f2ade679aa32a598c
Trip-kun <kaidenmcmartin1@gmail.com>
2024-03-04 22:03:36 -0500

    fix(curve): Honestly don't know what changed

--- src/main/java/frc/robot/control/curve/LimitedCurve.java
+++ src/main/java/frc/robot/control/curve/LimitedCurve.java
@@ -1,4 +1,17 @@
 package frc.robot.control.curve;
 
-public class LimitedCurve {
+public class LimitedCurve extends Curve {
+    private final Curve curve;
+    private double limit = 1.0;
+    public LimitedCurve(Curve curve, double limit) {
+        this.curve = curve;
+        this.limit = limit;
+    }
+    public LimitedCurve(Curve curve) {
+        this.curve = curve;
+    }
+    @Override
+    protected double internal_curve(double x) {
+        return limit*curve.curve(x);
+    }
 }
